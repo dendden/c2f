7a485f6e911cc16ce78bac9ce77efff44aaa5110
Denys Triasunov <dendden@users.noreply.github.com>
2015-08-31 18:20:31 +0300

    version 2.0
    this update adds interactive input for celsius values

--- c2f.rb
+++ c2f.rb
@@ -1,3 +1,2 @@
-celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
-puts "The result is: #{fahrenheit}."
+puts "Hello, please specify the celsius temperature:"
+puts "The result is: #{(gets.to_i * 9 / 5) + 32}."

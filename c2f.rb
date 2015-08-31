a5001dd43c4237452433793fa223c1e9978a1b13
Denys Triasunov <dendden@users.noreply.github.com>
2015-09-01 00:14:41 +0300

    version 2.2
    This version takes celsius value from user and writes the fahrenheit
    result to file «temp.out»

--- c2f.rb
+++ c2f.rb
@@ -1,8 +1,6 @@
-# puts "Hello, please specify the celsius temperature:"
-# puts "The result is: #{(gets.to_i * 9 / 5) + 32}."
-puts "Reading data from file..."
-num = File.read('temp.dat')
-celsius = num.to_i
-fahrenheit = celsius * 9 / 5 + 32
-puts "Celsius value is #{num}"
-puts "The result is #{fahrenheit}."
+puts "Hello, please specify the celsius temperature:"
+fahrenheit = gets.to_i * 9 / 5 + 32
+puts "Writing result to file 'temp.out'"
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close

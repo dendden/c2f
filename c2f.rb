65aa31f2b54b70e16c913d6a56a70b5f51973418
Denys Triasunov <dendden@users.noreply.github.com>
2015-09-01 00:08:18 +0300

    version 2.1
    This version reads celsius value from file «temp.dat»

--- c2f.rb
+++ c2f.rb
@@ -1,2 +1,8 @@
-puts "Hello, please specify the celsius temperature:"
-puts "The result is: #{(gets.to_i * 9 / 5) + 32}."
+# puts "Hello, please specify the celsius temperature:"
+# puts "The result is: #{(gets.to_i * 9 / 5) + 32}."
+puts "Reading data from file..."
+num = File.read('temp.dat')
+celsius = num.to_i
+fahrenheit = celsius * 9 / 5 + 32
+puts "Celsius value is #{num}"
+puts "The result is #{fahrenheit}."
